a003dc5e4218911d0ae6dfcc52211d67f3fcffd2
Brendan Till <brendan.till@gmail.com>
2014-01-11 14:05:18 -0800

    add stat_type option to include per game, per 36, total, and advanced

--- players.py
+++ players.py
@@ -3,46 +3,54 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def getSeasonStatSoup(year):
+def getSeasonStatSoup(year, stat_type='per game'):
 
-	# basketball-reference season stat page
-	base_url = 'http://www.basketball-reference.com/leagues/NBA_%(year)d_totals.html'
-	url = base_url % {'year': year}
+	stat_type = stat_type.lower()
+	stat_type_dict = {'per game': 'per_game', 'total': 'totals', 'per 36': 'per_minute', 'advanced': 'advanced'}
+	if stat_type not in stat_type_dict.keys():
+		print "Stat type not supported. Valid options: " + str(stat_type_dict.keys())
+	else:
 
-	# BeautifulSoup parsing only works with "xml" parsing option -- not exactly sure why
-	r = requests.get(url)
-	soup = BeautifulSoup(r.text, "xml")
+		# basketball-reference season stat page
+		base_url = 'http://www.basketball-reference.com/leagues/NBA_%(year)d_%(stat_type_str)s.html'
+		url = base_url % {'year': year, 'stat_type_str': stat_type_dict[stat_type]}
 
-	return soup
+		# BeautifulSoup parsing only works with "xml" parsing option -- not exactly sure why
+		r = requests.get(url)
+		soup = BeautifulSoup(r.text, "xml")
 
-def getPlayerYear(name, year):
+		return soup
 
-	soup = getSeasonStatSoup(year)
+def getPlayerYear(name, year, stat_type='per game'):
 
-	# get category strings -- this assumes same order as stats
-	cats = []
-	cat_parent = soup.find('tr', class_='no_ranker thead')
-	if cat_parent != None:
-		cats_html = cat_parent.find_all('th')
-		for cat in cats_html:
-			cats.append(cat.get('data-stat'))
+	soup = getSeasonStatSoup(year, stat_type)
 
-	# transform player name to "Last, First" format
-	name_list = name.split(' ')
-	name_list.reverse()
-	csk_str = ','.join(name_list)
+	if soup is not None:
 
-	# find player via csk_str, then iterate over stat entries
-	stats = []
-	tag = soup.find('td', csk=csk_str)
-	if tag != None:
-		stats_html = tag.parent.find_all('td')
-		for stat in stats_html:
-			# remove asterisk from some players
-			stats.append(re.sub('\*','',stat.get_text())) 
+		# get category strings -- this assumes same order as stats
+		cats = []
+		cat_parent = soup.find('tr', class_='no_ranker thead')
+		if cat_parent != None:
+			cats_html = cat_parent.find_all('th')
+			for cat in cats_html:
+				cats.append(cat.get('data-stat'))
 
-	player_stats = dict(zip(cats, stats))
-	return player_stats
+		# transform player name to "Last, First" format
+		name_list = name.split(' ')
+		name_list.reverse()
+		csk_str = ','.join(name_list)
+
+		# find player via csk_str, then iterate over stat entries
+		stats = []
+		tag = soup.find('td', csk=csk_str)
+		if tag != None:
+			stats_html = tag.parent.find_all('td')
+			for stat in stats_html:
+				# remove asterisk from some players
+				stats.append(re.sub('\*','',stat.get_text())) 
+
+		player_stats = dict(zip(cats, stats))
+		return player_stats
 
 def listPlayers(year):
 
--- players_test.py
+++ players_test.py
@@ -9,11 +9,11 @@ class TestPlayers(unittest.TestCase):
 		# run at start of each test
 
 	def test_getPlayerYear(self):
-		cases = [('Klay Thompson', 2013), ('Kobe Bryant', 2002), ('Michael Jordan', 1996), ('Player', 2013), ('Anthony Davis', 2000)]
+		cases = [('Klay Thompson', 2013, 'per game'), ('Kobe Bryant', 2002, 'advanced'), ('Michael Jordan', 1996, 'total'), ('Player', 2013, 'per game'), ('Anthony Davis', 2000, 'per 36')]
 		expected_success = [True, True, True, False, False]
 		for n, case in enumerate(cases):
-			print "Test case: " + case[0] + ',', case[1]
-			result = players.getPlayerYear(case[0], case[1])
+			print "Test case: " + case[0] + ', ' + str(case[1]) + ', ' + case[2]
+			result = players.getPlayerYear(case[0], case[1], case[2])
 			self.assertEqual(len(result) > 0, expected_success[n])
 
 	def test_listPlayers(self):
